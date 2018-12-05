e25a59b79bb49f415bc345da2642033cb2749da2
Nicocovi <nico0205@hotmail.com>
2018-12-05 13:34:03 +0100

    Sunburst in servicegraph

--- src/main/resources/static/js/sunburst.js
+++ src/main/resources/static/js/sunburst.js
@@ -1,17 +1,16 @@
 function drawSunburst(sunData, diameter){
 	// Variables
-	var size = $("#domain-graph").width();
+	var size = $("svg").width();
     var width = size;
     var height = size;
-    var radius = width / 2;
+    var radius = width / 4;
+    console.log("radius", radius);
     var color = d3.scaleOrdinal(d3.schemeCategory20b);
     console.log("Drawing sunburst");
     // Size our <svg> element, add a <g> element, and move translate 0,0 to the center of the element.
-    var g = d3.select('#domain-graph svg')
-        .attr('width', diameter)
-        .attr('height', diameter)
-        .append('g')
-        .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
+    var g = d3.select("#graph-cloudfoundry svg")
+    			.append('g')
+    			.attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
     // Create our sunburst data structure and size it.
     var partition = d3.partition().size([2 * Math.PI, radius]);
@@ -24,14 +23,29 @@ function drawSunburst(sunData, diameter){
     var arc = d3.arc()
         .startAngle(function (d) { return d.x0 })
         .endAngle(function (d) { return d.x1 })
-        .innerRadius(function (d) { return d.y0 })
-        .outerRadius(function (d) { return d.y1 });
+        .innerRadius(function (d) { 
+        	return d.y0
+        	})
+        .outerRadius(function (d) {
+        	return d.y1
+        	});
+    
+    function innerR(d) {
+    	console.log("Dy0", radius * Math.sqrt(d.y0) / 30);
+        return radius * Math.sqrt(d.y0) / 25;
+    }
+
+    function outerR(d) {
+    	console.log("Dy1", radius * Math.sqrt(d.y1) / 30 - 8);
+        return radius * Math.sqrt(d.y1) / 25 - 2;
+    }
 
     // Add a <g> element for each node in thd data, then append <path> elements and draw lines based on the arc
     // variable calculations. Last, color the lines and the slices.
     g.selectAll('g')
         .data(root.descendants())
         .enter().append('g').attr("class", "node").append('path')
+        .attr("class", "sunburst-path")
         .attr("display", function (d) { return d.depth ? null : "none"; })
         .attr("d", arc)
         .style('stroke', '#fff')
@@ -48,6 +62,10 @@ function drawSunburst(sunData, diameter){
         .text(function(d) { return d.parent ? d.data.name : "" });
 
 }
+
+d3.selection.prototype.first = function() {
+  return d3.select(this[0][0]);
+};
 /**
  * Calculate the correct distance to rotate each label based on its location in the sunburst.
  * @param {Node} d
